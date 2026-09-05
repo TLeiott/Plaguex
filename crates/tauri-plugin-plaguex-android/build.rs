0e89d9016807968e85439da5fbc3d37d5340263f
TLeiott <t.leiott@hmd-software.com>
2026-09-05 22:27:18 +0200

    style: rustfmt

--- crates/tauri-plugin-plaguex-android/build.rs
+++ crates/tauri-plugin-plaguex-android/build.rs
@@ -1,5 +1,9 @@
-const COMMANDS: &[&str] = &["set_orientation", "set_immersive", "set_fit_system_windows",
-    "share_file"];
+const COMMANDS: &[&str] = &[
+    "set_orientation",
+    "set_immersive",
+    "set_fit_system_windows",
+    "share_file",
+];
 
 fn main() {
     tauri_plugin::Builder::new(COMMANDS)
